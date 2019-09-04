0a1ca6adb2e9d19fc589218f2f57e04beae8a123
Jommy Belinda <jbelinda@student.21-school.ru>
2019-09-04 21:34:24 +0300

    ft_itoa implemented, ft_putnbr_fd influenced

---
+++ ft_itoa.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbelinda <jbelinda@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/04 19:49:23 by jbelinda          #+#    #+#             */
+/*   Updated: 2019/09/04 21:13:53 by jbelinda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define ABS(x)	((x) < 0 ? -(x) : (x))
+
+static int	ft_number_length(int n)
+{
+	int l;
+
+	l = n < 0 ? 1 : 0;
+	while (n)
+	{
+		++l;
+		n /= 10;
+	}
+	return (l ? l : 1);
+}
+
+char		*ft_itoa(int n)
+{
+	char	*str;
+	bool	neg;
+	int		l;
+
+	neg = (n < 0);
+	l = ft_number_length(n);
+	str = (char *)malloc(sizeof(char) * l + 1);
+	if (str)
+	{
+		str[l] = '\0';
+		if (n < 0)
+			str[0] = '-';
+		while (--l + (neg ? 0 : 1))
+		{
+			str[l] = ABS(n % 10) + '0';
+			n /= 10;
+		}
+	}
+	return (str);
+}
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: jbelinda <jbelinda@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/04 17:52:59 by jbelinda          #+#    #+#             */
-/*   Updated: 2019/09/04 18:05:31 by jbelinda         ###   ########.fr       */
+/*   Updated: 2019/09/04 21:16:51 by jbelinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,12 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < 0)
+	char *s;
+
+	s = ft_itoa(n);
+	if (s)
 	{
-		ft_putchar_fd('-', fd);
-		if (n == INT_MAX)
-		{
-			ft_putchar_fd('2', fd);
-			n = -147483648;
-		}
-		n = -n;
-	}
-	if (n < 10)
-		ft_putchar_fd(n + '0', fd);
-	else
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
+		ft_putstr_fd(s, fd);
+		free(s);
 	}
 }
