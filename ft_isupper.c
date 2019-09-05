6ad39da4137c0e12680860163de623bf042ae2b1
ash <alexander.naumochkin@gmail.com>
2019-09-06 00:53:55 +0300

    set of ft_is* functions

---
+++ ft_isupper.c
@@ -0,0 +1,4 @@
+int	ft_isupper(int c)
+{
+	return (('A' <= c) && (c <= 'Z'));
+}
--- libft.h
+++ libft.h
@@ -45,6 +45,7 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
 char	*ft_strnew(size_t size);
@@ -69,5 +70,8 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
+int		ft_isupper(int c);
+int 	ft_islower(int c);
 
 #endif
