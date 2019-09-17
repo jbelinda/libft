cf41d5771787e5d9469426a2cb9e9f49bbb35457
jommy belinda <jbelinda@student.21-school.ru>
2019-09-18 01:59:17 +0300

    casting size_t to ptrdiff_t :)

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,14 +6,16 @@
 /*   By: jbelinda <jbelinda@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 00:09:37 by jbelinda          #+#    #+#             */
-/*   Updated: 2019/09/18 01:28:30 by jbelinda         ###   ########.fr       */
+/*   Updated: 2019/09/18 01:58:19 by jbelinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stddef.h>
+#include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t		nlen;
-	size_t		slen;
 	char		*s;
 	const char	*sub;
 
@@ -21,11 +23,11 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		return ((char *)haystack);
 	sub = haystack;
 	while ((s = ft_strchr(sub, *needle)) != NULL)
-		if (((s - haystack) < len) &&
+		if (((s - haystack) < (long)len) &&
 			(ft_strlen(s) >= nlen) &&
 			(ft_strncmp(s, needle, nlen)) == 0)
 			return (s);
 		else
-			sub = s + 1;	
+			sub = s + 1;
 	return (NULL);
 }
