48676d37d60316dbf20b7db3e9bdf8d48852a977
jommy belinda <jbelinda@student.21-school.ru>
2019-10-11 15:04:26 +0300

    memjoin fix

--- ft_memjoin.c
+++ ft_memjoin.c
@@ -6,7 +6,7 @@
 /*   By: jbelinda <jbelinda@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 18:44:21 by jbelinda          #+#    #+#             */
-/*   Updated: 2019/09/18 18:44:24 by jbelinda         ###   ########.fr       */
+/*   Updated: 2019/10/11 15:03:51 by jbelinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 void	*ft_memjoin(const void *s1, size_t n1, const void *s2, size_t n2)
 {
 	void	*dst;
+	void	*d;
 
 	if ((dst = malloc(n1 + n2)))
 	{
+		d = dst;
 		if (n1)
-			dst = ft_memcpy(dst, s1, n1);
+			d = ft_memcpy(d, s1, n1);
 		if (n2)
-			dst = ft_memcpy(dst + (n1 ? 1 : 0), s2, n2);
+			d = ft_memcpy(d + n1, s2, n2);
 	}
 	return (dst);
 }
