df90aa0413b2b47423403e6216eeb49ed6a9e889
jommy belinda <jbelinda@student.21-school.ru>
2019-10-13 06:26:18 +0300

    const deconstize :)

--- ft_memrealloc.c
+++ ft_memrealloc.c
@@ -6,17 +6,17 @@
 /*   By: jbelinda <jbelinda@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 01:39:15 by jbelinda          #+#    #+#             */
-/*   Updated: 2019/10/13 01:49:01 by jbelinda         ###   ########.fr       */
+/*   Updated: 2019/10/13 06:25:30 by jbelinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memrealloc(void *p, size_t psize, size_t nsize)
+void	*ft_memrealloc(const void *p, size_t psize, size_t nsize)
 {
 	void	*new;
 
-	new = p;
+	new = (void *)p;
 	if (nsize)
 		if ((new = ft_memalloc(psize + nsize)) && p)
 			new = ft_memcpy(new, p, psize);
