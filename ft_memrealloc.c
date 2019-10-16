a1edcfb5e47e134618824f0a0827d62644c9ecfe
jommy belinda <jbelinda@student.21-school.ru>
2019-10-16 04:57:45 +0300

    cast to void ** in memrealloc

--- ft_memrealloc.c
+++ ft_memrealloc.c
@@ -6,7 +6,7 @@
 /*   By: jbelinda <jbelinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 01:39:15 by jbelinda          #+#    #+#             */
-/*   Updated: 2019/10/16 04:49:43 by jbelinda         ###   ########.fr       */
+/*   Updated: 2019/10/16 04:56:58 by jbelinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memrealloc(const void *p, size_t psize, size_t nsize)
 		if ((new = ft_memalloc(nsize)) && p)
 		{
 			new = ft_memcpy(new, p, ft_min(psize, nsize));
-			ft_memdel(&p);
+			ft_memdel((void **)&p);
 		}
 	return (new);
 }
