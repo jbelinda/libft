ab1975ee1375727e912f2a9e2149cde3f00b5fc6
jommy belinda <jbelinda@student.21-school.ru>
2019-10-16 04:51:07 +0300

    memrealloc frees old allocation on success realloc

--- ft_memrealloc.c
+++ ft_memrealloc.c
@@ -6,7 +6,7 @@
 /*   By: jbelinda <jbelinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 01:39:15 by jbelinda          #+#    #+#             */
-/*   Updated: 2019/10/16 04:43:20 by jbelinda         ###   ########.fr       */
+/*   Updated: 2019/10/16 04:49:43 by jbelinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@ void	*ft_memrealloc(const void *p, size_t psize, size_t nsize)
 	new = (void *)p;
 	if (nsize)
 		if ((new = ft_memalloc(nsize)) && p)
+		{
 			new = ft_memcpy(new, p, ft_min(psize, nsize));
+			ft_memdel(&p);
+		}
 	return (new);
 }
